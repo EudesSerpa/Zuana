ff2dd9b8fd08c8fd2f5636f65dad4550f17276dd
Eudes Serpa <48723112+EudesSerpa@users.noreply.github.com>
2021-05-21 14:14:51 -0500

    Delete src directory

--- src/DateFormat.py
+++
@@ -1,9 +0,0 @@
-"""
-    FORMATEO DE LA FECHA ACTUAL
-"""
-from datetime import datetime
-
-def currentDateFormatted():
-    date = datetime.now()
-    date = date.strftime('%Y-%m-%d %X')
-    return date
